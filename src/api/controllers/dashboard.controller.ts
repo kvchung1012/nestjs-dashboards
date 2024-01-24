08bbad58f420217fe774b4ce556fb35f8d83253d
boldnguyen <chungkv@quangich.com>
2024-01-24 17:49:23 +0700

    update

--- src/api/controllers/dashboard.controller.ts
+++ src/api/controllers/dashboard.controller.ts
@@ -255,25 +255,27 @@ export class DashboardController {
       },
     ]);
 
+    // lịch học của sinh viên lấy trong file diem_nguyen.xlsx
     const schedules = await this.scheduleModel.find({});
-
-    // tìm tổng học sinh theo nghành và khóa
-    const totalStudent = schedules.filter(
-      (x) =>
-        x.studentCode.toUpperCase().includes(major.toUpperCase()) &&
-        x.studentCode.toUpperCase().includes(enroll.toUpperCase()),
-    ).length;
 
     const results = course.map((x) => {
       return {
         group: x._id,
+        // tổng sinh viên học pass môn
         totalStudentLearned: schedules.filter(
+          (y) =>
+            y.studentCode.includes(major) &&
+            y.studentCode.includes(enroll) &&
+            y.course.includes(x._id) &&
+            y.score >= 50,
+        ).length,
+        // tổng sinh viên học course
+        totalStudent: schedules.filter(
           (y) =>
             y.studentCode.includes(major) &&
             y.studentCode.includes(enroll) &&
             y.course.includes(x._id),
         ).length,
-        totalStudent: totalStudent,
       };
     });
     return results;
